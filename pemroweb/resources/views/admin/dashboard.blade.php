b64bf9e2cd400e5b45058efc985325c04a02c957
unknown <adiwiratama27>
2021-04-29 01:03:22 +0800

    project

---
+++ pemroweb/resources/views/admin/dashboard.blade.php
@@ -0,0 +1,12 @@
+<x-template-layout>
+    <h1 class="text-3xl text-black pb-6">{{$title}}</h1>
+    <div class="w-full mt-12">
+        <div class="bg-white overflow-auto">
+            <img src = gambar1.jpg width= "600" height="250">
+            <table class="min-w-full bg-white">
+                <thead class="bg-gray-800 text-white">
+                </thead>
+            </table>
+        </div>
+    </div>
+</x-template-layout>
